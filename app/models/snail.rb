6a885e74cc67bf19d25b43908a6b1c6efd228526
Carl Schubert <carl.schubert@vts.com>
2020-09-09 15:18:15 -0400

    Fix in memory problem

--- app/models/snail.rb
+++ app/models/snail.rb
@@ -10,14 +10,6 @@ class Snail < ApplicationRecord
   end
 
   def longest_trail
-    longest_trail = nil
-    trails.each_with_index do |trail, index|
-      trails.each do |other_trail|
-        if trail.length >= other_trail.length
-          longest_trail = trail
-        end
-      end
-    end
-    longest_trail
+    trails.max_by(&:length)
   end
 end
